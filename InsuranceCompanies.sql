2efac22bb6e78603e1062d3d8b755a4aad52beec
Nastaran <nastaranashouri77@gmail.com>
2020-12-15 17:03:07 +0330

    Insurance company

---
+++ InsuranceCompanies.sql
@@ -0,0 +1,78 @@
+create or alter procedure InsuranceCompaniesFirstLoader
+as
+begin
+	begin try 
+		delete from HospitalDW.dbo.InsuranceCompanies
+		dbcc CHECKIDENT([HospitalDW.dbo.InsuranceCompanies],RESEED,0)
+		insert into HospitalDW.dbo.InsuranceCompanies
+        SELECT [insuranceCompany_ID]
+		,[name]
+		,[license_code]
+		,[phone_number]
+		,[address]
+		FROM HospitalSA.dbo.InsuranceCompanies
+	---------------------------------------------------
+		insert into [dbo].[Logs]
+			([date]
+			,[table_name]
+			,[status]
+			,[text]
+			,[affected_rows])
+		values
+			(GETDATE()
+			,'InsuranceCompanies'
+			,1
+			,'inserting new values was successfull'
+			,@@ROWCOUNT)
+	end try
+	begin catch
+		INSERT INTO HospitalDW.dbo.Logs([date], [table_name], [status], [text], [affected_rows])
+		VALUES (GETDATE(), 'InsuranceCompanies', 0, 'Error while inserting or updating', @@ROWCOUNT);
+        select ERROR_MESSAGE()
+	end catch
+end
+
+exec InsuranceCompaniesFirstLoader
+---------------------------------------------
+---------------------------------------------
+
+create or alter procedure InsuranceCompaniesLoader
+	as 
+	begin
+	begin try
+		merge HospitalDW.dbo.InsuranceCompanies as IC
+		using HospitalSA.dbo.InsuranceCompanies as SA on
+		IC.insuranceCompany_ID = SA.insuranceCompany_ID
+
+		when not matched then 
+		insert values (
+		[insuranceCompany_ID]
+		,[name]
+		,[license_code]
+		,[phone_number]
+		,[address]
+		)
+		when matched and (IC.phone_number != SA.phone_number) then 
+		update set IC.phone_Number = SA.phone_number
+		;
+		--logs
+		insert into [dbo].[Logs]
+			([date]
+			,[table_name]
+			,[status]
+			,[text]
+			,[affected_rows])
+		values
+			(GETDATE()
+			,'InsuranceCompanies'
+			,1
+			,'inserting new values was successfull'
+			,@@ROWCOUNT)
+	end try
+	begin catch
+		INSERT INTO HospitalDW.dbo.Logs([date], [table_name], [status], [text], [affected_rows])
+		VALUES (GETDATE(), 'dbo.InsuranceCompanies', 0, 'Error while inserting or updating', @@ROWCOUNT);
+	end catch
+end
+
+exec InsuranceCompaniesFirstLoader
