ca95dc614edf8dfadd340717355e7f720b3ce10e
kharmolak <76033512+kharmolak@users.noreply.github.com>
2020-12-15 15:12:04 +0330

    Update Medicines_Patients_SP.sql

--- Medicines_Patients_SP.sql
+++ Medicines_Patients_SP.sql
@@ -24,11 +24,12 @@ BEGIN
 			FROM HospitalSA.dbo.Patients;
 
 		INSERT INTO HospitalDW.dbo.Logs([date], [table_name], [status], [text], [affected_rows])
-		VALUES (GETDATE(), 'dbo.Patients', 1, 'Patients First Load was Successful', @@ROWCOUNT);
+		VALUES (GETDATE(), 'dbo.Patients', 1, 'First Load was Successful', @@ROWCOUNT);
 	END TRY
 	BEGIN CATCH
 		INSERT INTO HospitalDW.dbo.Logs([date], [table_name], [status], [text], [affected_rows])
-		VALUES (GETDATE(), 'dbo.Patients', 0, 'Patients First Load was Failed', @@ROWCOUNT);
+		VALUES (GETDATE(), 'dbo.Patients', 0, 'First Load was Failed', @@ROWCOUNT);
+		RETURN;
 	END CATCH
 END
 GO
@@ -36,41 +37,50 @@ GO
 CREATE OR ALTER PROCEDURE dbo.uspDimPatients
 AS 
 BEGIN 
-	MERGE INTO HospitalDW.dbo.Patients AS [Target]
-	USING HospitalSA.dbo.Patients AS [Source]
-	ON Source.patient_ID = [Target].patient_ID
-	WHEN MATCHED AND
-		 (
-			   [Target].height						<> Source.height
-			   OR [Target].[weight]				<> Source.[weight]
-			   OR [Target].phone_number <> Source.phone_number
-		 ) 
-	THEN UPDATE SET 
-				height = Source.[height],
-				[weight] = Source.[weight],
-				phone_number = Source.[phone_number]
-	WHEN NOT MATCHED BY TARGET 
-	THEN INSERT (
-		[patient_ID]
-		,[national_code]
-		,[name]
-		,[family]
-		,[birthdate]
-		,[height]
-		,[weight]
-		,[gender]
-		,[phone_number]
-	) VALUES (
-		Source.[patient_ID],
-		Source.[national_code],
-		Source.[first_name],
-		Source.[last_name],
-		Source.[birthdate],
-		Source.[height],
-		Source.[weight],
-		Source.[gender],
-		Source.[phone_number]
-	);
+	BEGIN TRY
+		MERGE INTO HospitalDW.dbo.Patients AS [Target]
+		USING HospitalSA.dbo.Patients AS [Source]
+		ON Source.patient_ID = [Target].patient_ID
+		WHEN MATCHED AND
+			 (
+				   [Target].height						<> Source.height
+				   OR [Target].[weight]				<> Source.[weight]
+				   OR [Target].phone_number <> Source.phone_number
+			 ) 
+		THEN UPDATE SET 
+					height = Source.[height],
+					[weight] = Source.[weight],
+					phone_number = Source.[phone_number]
+		WHEN NOT MATCHED BY TARGET 
+		THEN INSERT (
+			[patient_ID]
+			,[national_code]
+			,[name]
+			,[family]
+			,[birthdate]
+			,[height]
+			,[weight]
+			,[gender]
+			,[phone_number]
+		) VALUES (
+			Source.[patient_ID],
+			Source.[national_code],
+			Source.[first_name],
+			Source.[last_name],
+			Source.[birthdate],
+			Source.[height],
+			Source.[weight],
+			Source.[gender],
+			Source.[phone_number]
+		);
+		INSERT INTO HospitalDW.dbo.Logs([date], [table_name], [status], [text], [affected_rows])
+		VALUES (GETDATE(), 'dbo.Patients', 1, 'Update or Insert was Successful', @@ROWCOUNT);
+	END TRY
+	BEGIN CATCH
+		INSERT INTO HospitalDW.dbo.Logs([date], [table_name], [status], [text], [affected_rows])
+		VALUES (GETDATE(), 'dbo.Patients', 0, 'Update or Insert was Failed', @@ROWCOUNT);
+		RETURN;
+	END CATCH
 END
 GO
 
@@ -78,7 +88,33 @@ GO
 CREATE OR ALTER PROCEDURE Pharmacy.uspDimMedicinesFirstLoad
 AS 
 BEGIN
-	INSERT INTO HospitalDW.Pharmacy.Medicines
+	BEGIN TRY
+		INSERT INTO HospitalDW.Pharmacy.Medicines
+			SELECT [medicine_ID]
+						,[medicineFactory_ID]
+						,[name]
+						,[latin_name]
+						,[dose]
+						,[side_effects]
+						,[price]
+						,[description] 
+			FROM HospitalSA.dbo.Medicines;
+		INSERT INTO HospitalDW.dbo.Logs([date], [table_name], [status], [text], [affected_rows])
+		VALUES (GETDATE(), 'Pharmacy.Medicines', 1, 'First Load was Successful', @@ROWCOUNT);
+	END TRY
+	BEGIN CATCH
+		INSERT INTO HospitalDW.dbo.Logs([date], [table_name], [status], [text], [affected_rows])
+		VALUES (GETDATE(), 'Pharmacy.Medicines', 0, 'First Load was Failed', @@ROWCOUNT);
+		RETURN;
+	END CATCH
+END
+GO
+
+CREATE OR ALTER PROCEDURE Pharmacy.uspDimMedicines
+AS 
+BEGIN
+	BEGIN TRY
+		INSERT INTO HospitalDW.Pharmacy.Medicines
 		SELECT [medicine_ID]
 					,[medicineFactory_ID]
 					,[name]
@@ -86,16 +122,63 @@ BEGIN
 					,[dose]
 					,[side_effects]
 					,[price]
-					,[description] 
-		FROM HospitalSA.dbo.Medicines;
-END
-GO
-
-CREATE OR ALTER PROCEDURE Pharmacy.uspDimMedicines
-AS 
-BEGIN
-INSERT INTO HospitalDW.Pharmacy.Medicines
-SELECT [medicine_ID]
+					,[description]
+					,[start_date]
+					,[end_date]
+					,[current_flag]
+		FROM
+		(
+		  MERGE INTO  HospitalDW.Pharmacy.Medicines AS [Target]
+		  USING HospitalSA.dbo.Medicines AS Source
+		  ON  [Target].medicine_ID = Source.medicine_ID
+		  WHEN MATCHED AND
+			(
+				[Target].price <> Source. price AND 
+				[Target].current_flag = 1
+			)
+		  THEN UPDATE SET 
+				end_date = GETDATE(), 
+				current_flag = 0
+		  WHEN NOT MATCHED THEN  
+		  INSERT 
+		  (
+			[medicine_ID]
+			,[medicineFactory_ID]
+			,[name]
+			,[latin_name]
+			,[dose]
+			,[side_effects]
+			,[price]
+			,[description]
+		  )
+		  VALUES 
+		  (
+			Source.[medicine_ID], 
+			Source.[medicineFactory_ID],
+			Source.[name],
+			Source.[last_name],
+			Source.[dose],
+			Source.[side_effects],
+			Source.[price],
+			Source.[description]
+		  )
+		  OUTPUT $ACTION, 
+			Source.[medicine_ID], 
+			Source.[medicineFactory_ID],
+			Source.[name],
+			Source.[last_name],
+			Source.[dose],
+			Source.[side_effects],
+			Source.[price],
+			Source.[description],
+			GETDATE(),
+			NULL,
+			1
+		) 
+		AS CHANGES 
+		(
+			ACTION, 
+			[medicine_ID]
 			,[medicineFactory_ID]
 			,[name]
 			,[latin_name]
@@ -106,70 +189,15 @@ SELECT [medicine_ID]
 			,[start_date]
 			,[end_date]
 			,[current_flag]
-FROM
-(
-  MERGE INTO  HospitalDW.Pharmacy.Medicines AS [Target]
-  USING HospitalSA.dbo.Medicines AS Source
-  ON  [Target].medicine_ID = Source.medicine_ID
-  WHEN MATCHED AND
-	(
-		[Target].price <> Source. price AND 
-		[Target].current_flag = 1
-	)
-  THEN UPDATE SET 
-		end_date = GETDATE(), 
-		current_flag = 0
-  WHEN NOT MATCHED THEN  
-  INSERT 
-  (
-    [medicine_ID]
-	,[medicineFactory_ID]
-	,[name]
-	,[latin_name]
-	,[dose]
-	,[side_effects]
-	,[price]
-	,[description]
-  )
-  VALUES 
-  (
-    Source.[medicine_ID], 
-    Source.[medicineFactory_ID],
-    Source.[name],
-    Source.[last_name],
-    Source.[dose],
-	Source.[side_effects],
-	Source.[price],
-	Source.[description]
-  )
-  OUTPUT $ACTION, 
-    Source.[medicine_ID], 
-    Source.[medicineFactory_ID],
-    Source.[name],
-    Source.[last_name],
-    Source.[dose],
-	Source.[side_effects],
-	Source.[price],
-	Source.[description],
-    GETDATE(),
-    NULL,
-	1
-) 
-AS CHANGES 
-(
-	ACTION, 
-	[medicine_ID]
-	,[medicineFactory_ID]
-	,[name]
-	,[latin_name]
-	,[dose]
-	,[side_effects]
-	,[price]
-	,[description]
-	,[start_date]
-	,[end_date]
-	,[current_flag]
-)
-WHERE ACTION='UPDATE';
+		)
+		WHERE ACTION='UPDATE';
+		INSERT INTO HospitalDW.dbo.Logs([date], [table_name], [status], [text], [affected_rows])
+		VALUES (GETDATE(), 'Pharmacy.Medicines', 1, 'Update or Insert was Successful', @@ROWCOUNT);
+	END TRY
+	BEGIN CATCH
+		INSERT INTO HospitalDW.dbo.Logs([date], [table_name], [status], [text], [affected_rows])
+		VALUES (GETDATE(), 'Pharmacy.Medicines', 0, 'Update or Insert was Failed', @@ROWCOUNT);
+		RETURN;
+	END CATCH
 END
 GO
