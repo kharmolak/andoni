72761f30df5e7b4aa15b324eac8d38a7c080e17b
Nastaran <nastaranashouri77@gmail.com>
2020-12-15 16:23:41 +0330

    medicinefactory_procedure

---
+++ medicineFactoryDW_procedure.sql
@@ -0,0 +1,78 @@
+create or alter procedure MedicineFactoryLoader
+	as 
+	begin
+	begin try
+		merge HospitalDW.Pharmacy.MedicineFactories as MF
+		using HospitalSA.dbo.MedicineFactories as SA on
+		MF.MedicineFactory_ID = SA.MedicineFactory_ID
+
+		when not matched then 
+		insert values (
+		[medicineFactory_ID],
+		[name],
+		[license_code],
+		[phone_number]
+		)
+		when matched and (MF.phone_number != SA.phone_number) then 
+		update set MF.phone_Number = SA.phone_number
+		;
+		--logs
+		insert into [dbo].[Logs]
+			([date]
+			,[table_name]
+			,[status]
+			,[text]
+			,[affected_rows])
+		values
+			(GETDATE()
+			,'MedicineFactories'
+			,1
+			,'inserting new values was successfull'
+			,@@ROWCOUNT)
+	end try
+	begin catch
+		INSERT INTO HospitalDW.dbo.Logs([date], [table_name], [status], [text], [affected_rows])
+		VALUES (GETDATE(), 'dbo.MedicineFactories', 0, 'Error while inserting or updating', @@ROWCOUNT);
+	end catch
+end
+
+exec MedicineFactoryLoader
+
+------------------------------------------------------
+------------------------------------------------------
+
+create or alter procedure MedicineFactoryFirstLoader
+as
+begin
+	begin try 
+		delete from HospitalDW.Pharmacy.MedicineFactories
+		dbcc CHECKIDENT([HospitalDW.Pharmacy.MedicineFactories],RESEED,0)
+		insert into HospitalDW.Pharmacy.MedicineFactories 
+        select 
+		[medicineFactory_ID],
+		[name],
+		[license_code],
+		[phone_number]
+		from HospitalSA.dbo.MedicineFactories;
+	---------------------------------------------------
+		insert into [dbo].[Logs]
+			([date]
+			,[table_name]
+			,[status]
+			,[text]
+			,[affected_rows])
+		values
+			(GETDATE()
+			,'MedicineFactories'
+			,1
+			,'inserting new values was successfull'
+			,@@ROWCOUNT)
+	end try
+	begin catch
+		INSERT INTO HospitalDW.dbo.Logs([date], [table_name], [status], [text], [affected_rows])
+		VALUES (GETDATE(), 'dbo.MedicineFactories', 0, 'Error while inserting or updating', @@ROWCOUNT);
+        select ERROR_MESSAGE()
+	end catch
+end
+
+exec MedicineFactoryFirstLoader
