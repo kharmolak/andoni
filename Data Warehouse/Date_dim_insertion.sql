c6785f715f4c0e3ab79625bfa7320ac4dbf769c9
kharmolak <76033512+kharmolak@users.noreply.github.com>
2020-12-19 23:34:03 +0330

    Reformat Codes + Edit Dimentions(Truncate --foreign keys)

--- Data Warehouse/Date_dim_insertion.sql
+++ Data Warehouse/Date_dim_insertion.sql
@@ -1,5 +1,6 @@
 delete from dbo.[Date];
 
+-- Sajede
 bulk insert dbo.[Date]
 from 'E:\education files\db2\Project\Date.txt'
 with
@@ -8,3 +9,13 @@ with
 	CODEPAGE = '65001'
 );
 
+-- Maryam 
+bulk insert dbo.[Date]
+from '\\VBOXSVR\Virtual_Share\DataBase2\andoni\Data Warehouse\Date.txt'
+with
+(
+	fieldterminator = '\t',
+	CODEPAGE = '65001'
+);
+
+-- Nastaran ?
--- Data Warehouse/HospitalDW_DDL.sql
+++ Data Warehouse/HospitalDW_DDL.sql
@@ -13,40 +13,42 @@ go
 create schema Clinic;
 go
 /***********************************Table********************************/
-
 --Patients
 create table Patients(
-patient_ID int primary key,
-national_code varchar(15) not null,
-[name] varchar(30) not null,
-family varchar(30) not null,
-birthdate date not null,
-height int null,
-[weight] int null,
-gender varchar(15) Check (gender in('Male','Female','Bi_sexual')),
-phone_number varchar(25) not null
+    patient_ID      int primary key,
+    national_code   varchar(15) not null,
+    [name]          varchar(30) not null,
+    family          varchar(30) not null,
+    birthdate       date not null,
+    height          int null,
+    [weight]        int null,
+    gender          varchar(15),
+    phone_number    varchar(25) not null
 );
+
 --MedicineFactories
 create table Pharmacy.MedicineFactories(
-medicineFactory_ID int primary key,
-[name] varchar(75) not null,
-license_code varchar(25) not null,
-phone_number varchar(25) null
+    medicineFactory_ID  int primary key,
+    [name]              varchar(75) not null,
+    license_code        varchar(25) not null,
+    phone_number        varchar(25) null
 );
+
 --Medicines
 create table Pharmacy.Medicines(
-medicine_code int IDENTITY(1,1) primary key,
-medicine_ID int,
-[name] varchar(30) not null,
-latin_name varchar(75) not null,
-dose float not null,
-side_effects varchar(200) null,
-price int not null,
-[description] varchar(200) null,
-start_date date,
-end_date date,
-current_flag int,
+    medicine_code   int IDENTITY(1,1) primary key,
+    medicine_ID     int,
+    [name]          varchar(30) not null,
+    latin_name      varchar(75) not null,
+    dose            float not null,
+    side_effects    varchar(200) null,
+    price           int not null,
+    [description]   varchar(200) null,
+    start_date      date,
+    end_date        date,
+    current_flag    int
 );
+
 --date
 CREATE TABLE Date (
     TimeKey                     int primary key,
@@ -72,182 +74,194 @@ CREATE TABLE Date (
     PersianCalendarYear         int,
     CalendarSemester            int,
     PersianCalendarSemester     int
-   )
+);
+
 --Departments
 create table Departments(
-department_ID int primary key,
-[name] varchar(30) not null,
-[description] varchar(300) null
+    department_ID   int primary key,
+    [name]          varchar(30) not null,
+    [description]   varchar(300) null
 );
+
 --Doctors
 create table Doctors(
-doctor_ID int primary key,
-national_code varchar(15) not null,
-license_code varchar(25) not null,
-[name] varchar(30) not null,
-family varchar(75) not null,
-birthdate date null,
-phone_number varchar(25) not null,
+    doctor_ID       int primary key,
+    national_code   varchar(15) not null,
+    license_code    varchar(25) not null,
+    [name]          varchar(30) not null,
+    family          varchar(75) not null,
+    birthdate       date null,
+    phone_number    varchar(25) not null,
 );
+
 --InsuranceCompanies
 create table InsuranceCompanies(
-insuranceCompany_ID int primary key,
-[name] varchar(75) not null,
-license_code varchar(25) not null,
-phone_number varchar(25) null,
-[address] varchar(300) null
+    insuranceCompany_ID int primary key,
+    [name]              varchar(75) not null,
+    license_code        varchar(25) not null,
+    phone_number        varchar(25) null,
+    [address]           varchar(300) null
 );
 -------------------------------------------------
 -------------------------------------------------
 --MedicineTransactionFact
 create table Pharmacy.MedicineTransactionFact(
-patient_code int,
-insuranceCompany_code int,
-medicine_code int,
-medicine_ID int,
-medicineFactory_code int,
-TimeKey int NOT NULL,
-number_of_units_bought int,
-cost int
-)
---MedicineSnapshotFact
-create table Pharmacy.MedicineSnapshotFact(
-medicine_code int,
-medicine_ID int,
-TimeKey int NOT NULL,
-total_number_bought int,
-total_cost int,
-number_of_patients_bought int,
-medicineFactory_code int
-);
---MedicineAccumulativeFact
-create table Pharmacy.MedicineAccumulativeFact(
-medicine_code int,
-medicine_ID int,
-total_number_bought int,
-total_cost int,
-number_of_patients_bought int,
-medicineFactory_code int
+    patient_code            int,
+    insuranceCompany_code   int,
+    medicine_code           int,
+    medicine_ID             int,
+    medicineFactory_code    int,
+    TimeKey                 int NOT NULL,
+    number_of_units_bought  int,
+    cost                    int
 );
 
+--MedicineSnapshotFact
+create table Pharmacy.MedicineSnapshotFact(
+    medicine_code               int,
+    medicine_ID                 int,
+    TimeKey                     int NOT NULL,
+    total_number_bought         int,
+    total_cost                  int,
+    number_of_patients_bought   int,
+    medicineFactory_code        int
+);
+
+--MedicineAccumulativeFact
+create table Pharmacy.MedicineAccumulativeFact(
+    medicine_code               int,
+    medicine_ID                 int,
+    total_number_bought         int,
+    total_cost                  int,
+    number_of_patients_bought   int,
+    medicineFactory_code        int
+);
 -------------------------------------------------------------
 -------------------------------------------------------------
 --AppointmentTransactionFact
 create table Clinic.AppointmentTransactionFact (
-patient_code int,
-insuranceCompany_code int,
-doctor_code int,
-TimeKey int NOT NULL,
-diagnosis varchar(300) null,
-[status] int not null,
-department_code int,
-price int not null
-)
+    patient_code            int,
+    insuranceCompany_code   int,
+    doctor_code             int,
+    TimeKey                 int NOT NULL,
+    diagnosis               varchar(300) null,
+    [status]                int not null,
+    department_code         int,
+    price                   int not null
+);
+
 --AppointmentSnapshotFact
 create table Clinic.AppointmentSnapshotFact (
-doctor_code int,
-TimeKey int NOT NULL,
-department_code int,
-total_price int not null,
-number_of_patient int
-)
+    doctor_code         int,
+    TimeKey             int NOT NULL,
+    department_code     int,
+    total_price         int not null,
+    number_of_patient   int
+);
+
 --AppointmentAccumulativeFact
 create table Clinic.AppointmentAccumulativeFact (
-doctor_code int,
-department_code int,
-total_price int not null,
-number_of_patient int
-)
+    doctor_code         int,
+    department_code     int,
+    total_price         int not null,
+    number_of_patient   int
+);
 
 -----------------------------------------------
 ----------------------------------------------
 --SurgeryTransactionFact
 create table Hospital.SurgeryTransactionFact(
-doctor_code int,
-patient_code int,
-insuranceCompany_code int,
-start_date int,
-end_date int,
-price int not null,
-[status] int not null,
-department_code int
-)
+    doctor_code             int,
+    patient_code            int,
+    insuranceCompany_code   int,
+    start_date              int,
+    end_date                int,
+    price                   int not null,
+    [status]                int not null,
+    department_code         int
+);
+
 --SurgerySnapShotFact
 create table Hospital.SurgerySnapshotFact(
-doctor_code int,
-department_code int,
-TimeKey int,
-total_price int not null,
-number_of_surgeries int,
-number_of_successful_surgeries int,
-number_of_failed_surgeries int
-)
+    doctor_code                     int,
+    department_code                 int,
+    TimeKey                         int,
+    total_price                     int not null,
+    number_of_surgeries             int,
+    number_of_successful_surgeries  int,
+    number_of_failed_surgeries      int
+);
+
 --SurgeryAccumulativeFact
 create table Hospital.SurgeryAccumulativeFact(
-doctor_code int,
-department_code int,
-total_price int not null,
-number_of_surgeries int,
-number_of_successful_surgeries int,
-number_of_failed_surgeries int
-)
+    doctor_code                     int,
+    department_code                 int,
+    total_price                     int not null,
+    number_of_surgeries             int,
+    number_of_successful_surgeries  int,
+    number_of_failed_surgeries      int
+);
+
 --HospitalTransactionFact
 create table Hospital.HospitalTransactionFact(
-doctor_code int,
-patient_code int,
-insuranceCompany_code int,
-department_code int,
-admit_date int not null,
-discharg_date int not null,
-room_number int null,
-daily_price int not null,
-total_price int not null,
-hospitalization_days int
-)
+    doctor_code             int,
+    patient_code            int,
+    insuranceCompany_code   int,
+    department_code         int,
+    admit_date              int not null,
+    discharg_date           int not null,
+    room_number             int null,
+    daily_price             int not null,
+    total_price             int not null,
+    hospitalization_days    int
+);
+
 --HospitalSnapshotFact
 create table Hospital.HospitalSnapshotFact(
-doctor_code int,
-department_code int,
-TimeKey int not null,
-total_patients_in_hospital int,
-patients_discharged int,
-patients_in_hospital int,
-total_price int not null
-)
+    doctor_code                 int,
+    department_code             int,
+    TimeKey                     int not null,
+    total_patients_in_hospital  int,
+    patients_discharged         int,
+    patients_in_hospital        int,
+    total_price                 int not null
+);
+
 --HospitalAccumulativeFact
 create table Hospital.HospitalAccumulativeFact(
-doctor_code int,
-department_code int,
-total_patients_in_hospital int,
-patients_discharged int,
-patients_in_hospital int,
-total_price int not null
-)
+    doctor_code                 int,
+    department_code             int,
+    total_patients_in_hospital  int,
+    patients_discharged         int,
+    patients_in_hospital        int,
+    total_price                 int not null
+);
 -----------------------------------------------
 ----------------------------------------------
 create table Hospital.SurgeryStatus(
-status_ID int primary key,
-[description] varchar(100)
+    status_ID       int primary key,
+    [description]   varchar(100)
 );
-create table Clinic.AppointmentStatus(
-status_ID int primary key,
-[description] varchar(100)
-);
-insert into Hospital.SurgeryStatus values
-(0,'Not Successful'),
-(1,'Successful');
-insert into Clinic.AppointmentStatus values
-(0,'Canseled'),
-(1,'Done');
 
+create table Clinic.AppointmentStatus(
+    status_ID       int primary key,
+    [description]   varchar(100)
+);
+
+insert into Hospital.SurgeryStatus values
+    (0,'Not Successful'),
+    (1,'Successful');
+
+insert into Clinic.AppointmentStatus values
+    (0,'Canseled'),
+    (1,'Done');
 -----------------------------------------------
 ----------------------------------------------
 --logtable
-create table Logs
-(
-    date       datetime,
-    table_name varchar(50),
-    status     tinyint,
-    text       varchar(500),
-    affected_rows int,
-)
+create table Logs(
+    date            datetime,
+    table_name      varchar(50),
+    status          tinyint,
+    text            varchar(500),
+    affected_rows   int,
+);
