ac71f4f2f45744e613041c4553bee98ae2301b72
kharmolak <76033512+kharmolak@users.noreply.github.com>
2021-01-31 13:00:10 +0330

    patient update

--- Data Warehouse/HospitalDW_DDL_V4.sql
+++ Data Warehouse/HospitalDW_DDL_V4.sql
@@ -11,6 +11,11 @@ go
 create database HospitalDW
 go 
 
+use HospitalDW
+go
+
+exec sp_changedbowner 'sa'
+go
 /***********************************Schema******************************/
 create schema Pharmacy
 go
@@ -23,84 +28,113 @@ go
 /********************************Dimensions*****************************/
 create table dimInsuranceCompanies(
 	insuranceCompany_ID		int primary key,
-	[name]								varchar(75) not null,
-	license_code					varchar(25) not null,
-	phone_number				varchar(25) null,--SCD1
-	[address]							varchar(300) null
+	[name]								varchar(75),
+	license_code					varchar(25),
+	phone_number				varchar(25),--SCD1
+	[address]							varchar(300),--SCD1
+	manager							varchar(100),--SCD3
+	agent								varchar(100),--SCD3
+	fax_number						varchar(25),--SCD1
+	website_address				varchar(200),
+	manager_phone_number varchar(25),--SCD1
+	agent_phone_number		varchar(25),--SCD1
+	additional_info					varchar(200),
+	active								bit,
+	active_description			varchar(200)
 );
 
 create table dimInsurances(
 	insurance_ID						int primary key,
-	insuranceCompany_ID			int not null,
-	insuranceCompany_name	varchar(75) not null,
-	code										varchar(15) not null,
-	expire_date							date null,
-	medicine_reduction				int not null,
-	appointment1_reduction		int not null,
-	appointment2_reduction		int not null,
-	appointment3_reduction		int not null,
-	hospitalization_reduction	int not null,
-	surgery_reduction				int not null,
-	test_reduction						int not null,
-	dentistry_reduction				int not null,
-	radiology_reduction			int not null,
+	code										varchar(15), 
+	insuranceCompany_ID			int,
+	insuranceCompany_name	varchar(75),
+	insurer									varchar(100),
+	insurer_phone_number		varchar(25),
+	additional_info						varchar(200),
+	expire_date							date,--SCD1
+	medicine_reduction				int,--SCD1
+	appointment1_reduction		int,--SCD1
+	appointment2_reduction		int,--SCD1
+	appointment3_reduction		int,--SCD1
+	hospitalization_reduction	int,--SCD1
+	surgery_reduction				int,--SCD1
+	test_reduction						int,--SCD1
+	dentistry_reduction				int,--SCD1
+	radiology_reduction			int,--SCD1
 );
 
 create table dimPatients(
-    patient_ID			int primary key,
-    national_code		varchar(15) not null,
-	insurance_ID		int not null,
-    [name]					varchar(30) not null,
-    family					varchar(30) not null,
-    birthdate				date not null,
-    height					int null,--SCD1
-    [weight]				int null,--SCD1
-    gender					varchar(15),
-    phone_number	varchar(25) not null,
-	death_date			date null,--SCD1
-	death_reason		int null,--SCD1
+    patient_ID							int primary key,
+    national_code						varchar(15),
+	insurance_ID						int,
+    first_name							varchar(30),
+    last_name							varchar(60),
+    birthdate								date,
+    height									int,--SCD1
+    [weight]								int,--SCD1
+    gender									varchar(15),
+    phone_number					varchar(25),--SCD1
+	death_date							date,--SCD1
+	death_reason						int,--SCD1
+	postal_code							varchar(20),--SCD1
+	[address]								varchar(200),--SCD1
+	additional_info						varchar(200),
 );
 
 create table Pharmacy.dimMedicineFactories(
-    medicineFactory_ID  int primary key,
-    [name]						 varchar(75) not null,
-    license_code			 varchar(25) not null,
-    phone_number		 varchar(25) null
+    medicineFactory_ID				int primary key,
+    [name]									varchar(75),
+    license_code						varchar(25),
+    phone_number					varchar(25),--SCD1
+	manager								varchar(100),--SCD3
+	agent									varchar(100),--SCD3
+	fax_number							varchar(25),--SCD1
+	website_address					varchar(200),
+	manager_phone_number	varchar(25),--SCD1
+	agent_phone_number			varchar(25),--SCD1
+	[address]								varchar(200),--SCD1
+	additional_info						varchar(200),
+	active									bit,
+	active_description				varchar(200)
 );
 
 create table Pharmacy.dimMedicines(
-    medicine_code	int IDENTITY(1,1) primary key,
-    medicine_ID			int,
-    [name]					varchar(30) not null,
-    latin_name			varchar(75) not null,
-    dose						float not null,
-    side_effects			varchar(200) null,
-    purchase_price		int not null,--SCD2
-	sales_price			int not null,--SCD2
-	stock					int not null,--SCD3
-	medicine_type		int not null, 
-    [description]		varchar(200) null,
-    [start_date]				date,
-    end_date				date,
-    current_flag			int
+    medicine_code						int identity(1,1) primary key, --surrogate key
+    medicine_ID								int,
+    [name]										varchar(30),
+    latin_name								varchar(75),
+    dose											float,
+    side_effects								varchar(200),
+    purchase_price							int,--SCD2
+	sales_price								int,--SCD2
+	stock										int,--SCD3 --> SCD1 ?
+	medicine_type							int, 
+	medicine_type_description		varchar(20),
+	medicineFactory_ID					int,
+	production_date						date,--SCD1
+	expire_date								date,--SCD1
+	additional_info							varchar(200),
+    [start_date]								date,
+    end_date									date,
+    current_flag								int,
 );
 
 create table dimDate (
     TimeKey										int primary key,
-    FullDateAlternateKey					varchar(max),
-    PersianFullDateAlternateKey		varchar(max),
+    FullDateAlternateKey					varchar(50),
+    PersianFullDateAlternateKey		varchar(50),
     DayNumberOfWeek					int,
     PersianDayNumberOfWeek			int,
-    EnglishDayNameOfWeek			varchar(max),
-    PersianDayNameOfWeek			nvarchar(max),
+    EnglishDayNameOfWeek			varchar(10),
+    PersianDayNameOfWeek			nvarchar(10),
     DayNumberOfMonth					int,
     PersianDayNumberOfMonth		int,
     DayNumberOfYear						int,
     PersianDayNumberOfYear			int,
     WeekNumberOfYear					int,
     PersianWeekNumberOfYear		int,
-    EnglishMonthName						varchar(max),
-    PersianMonthName					nvarchar(max),
+    EnglishMonthName						varchar(50),
+    PersianMonthName					nvarchar(50),
     MonthNumberOfYear					int,
     PersianMonthNumberOfYear		int,
     CalendarQuarter							int,
@@ -112,29 +146,45 @@ create table dimDate (
 );
 
 create table Clinic.dimDepartments(
-    department_ID		int primary key,
-    [name]					varchar(30) not null,
-    [description]		varchar(300) null
+    department_ID						int primary key,
+    [name]									varchar(30),
+    [description]						varchar(300),
+	chairman								varchar(50),--SCD3
+	assistant								varchar(50),--SCD3
+	chairman_phone_number	varchar(15),--SCD1
+	assistant_phone_number		varchar(15),--SCD1
+	chairman_room					int,
+	assistant_room						int,
+	reception_phone_number	varchar(15),--SCD1
+	budget									int,
+	additional_info						varchar(200)
 );
 
 create table Clinic.dimDoctors(
-    doctor_ID						int primary key,
-    national_code				varchar(15) not null,
-    license_code				varchar(25) not null,
-    [name]							varchar(30) not null,
-    family							varchar(75) not null,
-    birthdate						date null,
-    phone_number			varchar(25) not null,
-	department_ID				int not null,
-	department_name		varchar(30) not null,
-	education_degree		int not null, --[1-3]  --SCD2
-	specialty_description	varchar(100) null,
-	graduation_date			date not null,
-	university						varchar(100) null,
-	contract_start_date		date not null,
-	contract_end_date		date not null,
-	appointment_portion	int not null,
-	surgery_portion			int not null
+    doctor_ID								int primary key,
+    national_code						varchar(15),
+    license_code						varchar(25),
+    first_name							varchar(30),
+    last_name							varchar(75),
+    birthdate								date,
+    phone_number					varchar(25),--SCD1
+	department_ID						int,
+	department_name				varchar(30),
+	education_degree				int, --[1-3]  --SCD2 
+	specialty_description			varchar(100),
+	graduation_date					date,
+	university								varchar(100),
+	contract_start_date				date, ---?
+	contract_end_date				date,
+	appointment_portion			int,
+	gender									varchar(10),
+	religion								varchar(100),
+	nationality							varchar(50),
+	marital_status						bit,
+	marital_status_description	varchar(20), -- 0 for single / 1 for married
+	postal_code							varchar(12),
+	[address]								varchar(200),
+	additional_info						varchar(200),
 );
 
 create table Clinic.dimIllnessTypes(
@@ -146,21 +196,25 @@ create table Clinic.dimIllnessTypes(
 
 create table Clinic.dimIllnesses(
 	illness_ID					int primary key,
-	[name]						varchar(100) not null,
+	[name]						varchar(100),
 	illnessType_ID			int,
 	illnessType_name		varchar(50),
-	scientific_name		varchar(100) not null,
-	danger						smallint not null --[1,5]
+	scientific_name		varchar(100),
+	special_illness			bit, --0 for not special / 1 for special illnesses
+	killing_status			smallint,
+	killing_description	varchar(100),
+	chronic						bit,
+	chronic_description varchar(100)
 );
 
 /***********************************Facts*********************************/
 create table Pharmacy.factTransactionalMedicine(
-    patient_ID							int NOT NULL,
-    insuranceCompany_ID			int NOT NULL,
-    medicine_code					int NOT NULL,
-    medicine_ID							int ,
-    medicineFactory_ID				int NOT NULL,
-    TimeKey								int NOT NULL,
+    patient_ID							int,
+    insuranceCompany_ID			int,
+    medicine_code					int,
+    medicine_ID							int,
+    medicineFactory_ID				int,
+    TimeKey								int,
     number_of_units_bought	int,
     paied_price							int,
 	real_price								int,
@@ -326,7 +380,7 @@ create table Clinic.AppointmentStatus(
     (1,'Successful');
 */
 insert into Clinic.AppointmentStatus values
-    (0,'Canseled'),
+    (0,'Canceled'),
     (1,'Done');
 -----------------------------------------------
 ----------------------------------------------
